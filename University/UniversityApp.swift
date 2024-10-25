c59342cf70508d14003b940d85116d160ec76252
SrhtGurel <Serhat.Gurel@hotmail.com>
2024-10-25 18:02:13 +0300

    Initial Commit

---
+++ University/UniversityApp.swift
@@ -0,0 +1,17 @@
+//
+//  UniversityApp.swift
+//  University
+//
+//  Created by Serhat Gürel  on 25.10.24.
+//
+
+import SwiftUI
+
+@main
+struct UniversityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UniversityTests/UniversityTests.swift
@@ -0,0 +1,16 @@
+//
+//  UniversityTests.swift
+//  UniversityTests
+//
+//  Created by Serhat Gürel  on 25.10.24.
+//
+
+import Testing
+
+struct UniversityTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
